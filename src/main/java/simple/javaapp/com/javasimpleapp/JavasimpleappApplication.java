c8ab2bafeb614564a7987b34760e603caf4d8b60
anilp02 <anilkumarp31@gmail.com>
2018-06-11 06:02:45 -0400

    initial

---
+++ src/main/java/simple/javaapp/com/javasimpleapp/JavasimpleappApplication.java
@@ -0,0 +1,12 @@
+package simple.javaapp.com.javasimpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavasimpleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavasimpleappApplication.class, args);
+	}
+}
